9c9464872e6816afcb3ebc6f919eff18112a4f4a
kahlilbausley <42008951+kahlilbausley@users.noreply.github.com>
2023-11-06 12:13:57 -0800

    testing badge

---
+++ Develop/index.js
@@ -0,0 +1,122 @@
+// TODO: Include packages needed for this application
+var fs = require('fs');
+
+const prompt = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {   
+        type: "input",
+        name: "title",
+        message: "What is the title of the project?"
+    },
+{
+
+    type: "input",
+    name: "description",
+    message:"What is the description?",
+},
+
+{    
+    type: "input",
+    name: "installation",
+    message:"What are the installation instructions?",
+},
+{
+    type: "input",
+    name: "usage",
+    message:"What is the usage information?",
+},
+{
+    type: "input",
+    name: "guidelines",
+    message:"What are the contribution guidelines?",
+},
+{
+    type: "input",
+    name: "test",   
+    message:"What are the test instructions?",
+},
+{
+    type: "list",
+    name: "license",   
+    message:"What is the license?",
+    choices: [
+    "apache","MIT","GUN","BSD","LGPL","Mozilla",
+
+    ]
+},
+{
+    type: "input",
+    name: "username",     
+    message:"What is your GitHub username?",
+},
+{
+    type: "input",
+    name: "email",  
+    message:"What is your email address?",
+},
+
+
+];
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.appendFile(fileName,questions[0],function(){
+
+
+    });
+
+
+}
+
+// TODO: Create a function to initialize app
+
+function init() {
+    prompt.prompt(questions)
+    .then(answers=>{
+
+        fs.writeFileSync("readme.md",`
+        # ${answers.title}
+
+        ## description
+        ${answers.description}
+
+        
+
+        ## Installation
+        ${answers.installation}
+
+        ## Usage
+        ${answers.usage}
+
+        ## Guidelines
+        ${answers.guidelines}
+
+        ## Test
+        ${answers.test}
+
+        ## License
+        ${answers.license}
+        ![badmath](https://img.shields.io/badge/-blue)
+
+        ## Questions
+        https://github.com/${answers.username}
+        you can also reach me at ${answers.email}
+
+        ![license badge](./img/${answers.license}.jpeg)
+    `)});
+
+}
+
+
+
+
+
+
+// Function call to initialize app
+init();
+
---
+++ Develop/readme.md
@@ -0,0 +1,30 @@
+
+        # l
+
+        ## description
+        l
+
+        
+
+        ## Installation
+        l
+
+        ## Usage
+        l
+
+        ## Guidelines
+        l
+
+        ## Test
+        l
+
+        ## License
+        MIT
+        ![badmath](https://img.shields.io/badge/mit-blue)
+
+        ## Questions
+        https://github.com/l
+        you can also reach me at l
+
+        ![license badge](./img/MIT.jpeg)
+    
